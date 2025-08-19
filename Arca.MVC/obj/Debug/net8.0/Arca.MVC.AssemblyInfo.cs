c6d7af0d1ddce67b44a1d608e3cd72df2cf61c35
CespedesChris <chris_cespedes_04@yahoo.com>
2025-08-18 23:22:54 -0600

    Carga Incial

--- Arca.MVC/obj/Debug/net8.0/Arca.MVC.AssemblyInfo.cs
+++ Arca.MVC/obj/Debug/net8.0/Arca.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arca.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06dc99308bad17d36f8870858b6a2a946762ceaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc86d88449ed8d19b4cc97499d5c6cc2bb8df3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arca.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arca.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
